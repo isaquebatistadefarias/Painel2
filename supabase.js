550c73f4b570b3ad6cf1d86d3365e7ddbe62a71a
isaquebatistadefarias <125517838+isaquebatistadefarias@users.noreply.github.com>
2025-07-17 21:11:32 -0400

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,16 @@
+
++const { createClient } = supabase;
++const SUPABASE_URL = "https://wrayfqfxrxizltezkjrr.supabase.co";
++const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndyYXlmcWZ4cnhpemx0ZXpranJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3ODA2ODksImV4cCI6MjA2ODM1NjY4OX0.OP6XzwZ840PVj_0Oah0sSOfalWk5SH7xBDcQPVtaT8s";
++window.supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
++window.checkLogin = async function() {
++  const { data } = await supabaseClient.auth.getUser();
++  if (!data.user) {
++    window.location.href = "login.html";
++  }
++};
++window.logout = function() {
++  supabaseClient.auth.signOut().then(() => {
++    window.location.href = "login.html";
++  });
++};
